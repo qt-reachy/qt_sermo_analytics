a5e292ddcbda2cd4b83b12ec2467add07aaddec6
Kristoffer Kuwaja <kristoffer.kuvaja@gmail.com>
2024-08-19 18:13:54 +0300

    initial layout and dataload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # QT_Whisper_Analytics
 PlotlyDash Flask server for analyzing QT_Whisper data.
+
+## Tabs or Pages
+- Findings online point out that tabs are more seemless but for large datasets it can get slow.
+- Tabs are not performative for print/pdf conversion
+
+# Bugs
+## Major
+
+## Minor
+- There is a blue shadow over select button; find dbc bootstrap class
+
+- Disable clear all or change button to only select all?
--- app.py
+++ app.py
@@ -1,56 +1,81 @@
 # Import packages
 from dash import Dash, dcc, html, Input, Output, State, callback
-import functions
-from functions import BuildDataFrame
+import dash_bootstrap_components as dbc
+from functions import UsersDataFrame
 
 # Incorporate data
-Users = BuildDataFrame('logs.csv')
-data = Users.main()
+Users = UsersDataFrame('logs.csv')
 
 # Initialize the app
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = 'QT Analytics'
 
 # Custom variables
 title = '''
-# QT Analtics
-Analytics for QT robot @ArcadaUAS
+# QT Analtics @ArcadaUAS
 '''
 
 credits = '''
 Made by Kristoffer Kuvaja Adolfsson, ArcadaUAS 2024
 '''
 
-DropDown = data['user'].tolist()
-
-Actions = data.loc[:, data.columns != 'user'].columns.values.tolist()
+DropDown = Users.get_users()
+Actions = Users.get_actions()
 
 # App layout
-app.layout = html.Div(children=[
-    dcc.Markdown(children=title, style={'textAlign': 'center'}),
-    # top div
-    html.Div([
-        # Filter by user
-        html.Button("Select/Unselect All", id="select-all-users", n_clicks=0),
-        html.Div([
-            dcc.Dropdown(
-            options=DropDown,
-            value=DropDown,
-            multi=True,
-            id='filter-user'
-        )
+app.layout = dbc.Container(children=[
+    dbc.Row([
+        dbc.Col(html.Img(src="assets/arcada_crop.png",
+                style={"max-width": 250}), width=4),
+        dbc.Col(html.H1(children=['QT Analtics App']),
+                width=8, style={"textAlign": "right", "font-family": "futura-pt, fallback, arial, sans-serif"}),
+    ], style={"max-height": 200, "padding": "2vh"}, justify='start'),
+    dbc.Tabs([
+        dbc.Tab(label="Selector", children=[
+            dbc.Row([
+                dcc.Dropdown(
+                    options=DropDown,
+                    value=DropDown,
+                    multi=True,
+                    id='filter-user'
+                )
+            ]),
+            dbc.Row([
+                dbc.Col(dbc.Button("Select/Unselect All",
+                                   id="select-all-users", n_clicks=0), width=4)
+            ]),
+            dbc.Row([
+                dbc.Checklist(
+                    options=Actions,
+                    value=Actions,
+                    switch=True
+                )
+            ])
         ]),
-        html.Div([
-            dcc.Checklist(
-                options=Actions,
-                value=Actions,
-                inline=True
+        dbc.Tab(label="Graphs", children=[
+            dcc.Graph(
+                figure={
+                    'data': [
+                        {'x': [1, 2, 3], 'y': [4, 1, 2],
+                            'type': 'bar', 'name': 'SF'},
+                        {'x': [1, 2, 3], 'y': [2, 4, 5],
+                         'type': 'bar', 'name': 'Montréal'},
+                    ]
+                }
             )
-        ])
-    ]),
+        ]),
+        dbc.Tab(label="Table", children=[
+            html.Div(id='user-table')
+        ]),
+        dbc.Tab(label="Raw", children=[
+            dbc.Table.from_dataframe(
+                Users.get_raw_data(), bordered=True, hover=True)
+        ]),
+    ])
 ])
 
-@app.callback(
+
+@ app.callback(
     Output("filter-user", "value"),
     [Input("select-all-users", "n_clicks")],
     [State("filter-user", "value")])
@@ -59,6 +84,17 @@ def select_all_users(n_clicked, value):
         return []
     else:
         return DropDown
+
+
+@ app.callback(
+    Output('user-table', 'children'),
+    [Input('filter-user', 'value')],
+)
+def update_user_table(value):
+    # _df = Users.get_user_dataframe().loc[Users.get_user_dataframe()['user'].isin(value)]
+    _df = Users.get_exploded_user(value)
+    return dbc.Table.from_dataframe(_df, bordered=True, hover=True)
+
 
 # Run the app
 if __name__ == '__main__':
